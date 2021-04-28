349ad0aea9cd6ba36b7443717f1952e275c58428
meera096 <56079507+meera096@users.noreply.github.com>
2021-04-28 21:32:09 +0530

    Create band-name-generator.py

---
+++ band-name-generator.py
@@ -0,0 +1,4 @@
+print('WELCOME TO THE BRAND NAME MANAGER')
+city=input('Please enter the name of the city you live in\n')
+name=input('Please enter the name of your pet\n')
+print("Your band name is "+city +" "  +name)
